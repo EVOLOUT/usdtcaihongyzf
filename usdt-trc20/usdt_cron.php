8c68e871fc4cf93463486571ec5c8ad97bbcd032
EVOLOUT <103572070+EVOLOUT@users.noreply.github.com>
2022-04-13 18:53:56 +0800

    Update usdt_cron.php

--- usdt-trc20/usdt_cron.php
+++ usdt-trc20/usdt_cron.php
@@ -4,7 +4,7 @@ set_time_limit(0);//
 $interval=3;//
 $trade_no = $_GET['trade_no'];
 do{
-    $yz = file_get_contents("http://104.208.68.20:4801/usdt-trc20/cron.php?trade_no=$trade_no");//
+    $yz = file_get_contents("http://".$_SERVER['HTTP_HOST']."/usdt-trc20/cron.php?trade_no=$trade_no");//
     if ($yz == "USDT-TRC20 收款成功") {
         echo "yes";//
         exit();
